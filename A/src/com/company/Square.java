c7d2cd93cbc54647fea94705b64176da9ef45610
Andydorsty <andrewsacheampong1@gmail.com>
2021-10-20 08:44:13 +0000

    Addition Of Comments of Folder A

--- A/src/com/company/Square.java
+++ A/src/com/company/Square.java
@@ -2,10 +2,14 @@ package com.company;
 
 public class Square extends Shape{
 
+    //The square constructor, which is an inheritor of the methods in the Square class..
+    //automatically inherits the constructor
     public Square(Color color) {
         super(color);
     }
 
+
+    //the method in the Shape class, inherited by the square(a subclass of Shape)
     @Override
     public String draw() {
         return "Square drawn. " + color.fill();
--- A/src/com/company/SquareTest.java
+++ A/src/com/company/SquareTest.java
@@ -7,8 +7,12 @@ class SquareTest {
     @org.junit.jupiter.api.Test
     void whenBridgePatternInvoked_theConfigSuccess() {
 
+        //An object(a typeof a square) of a square class being created
         Shape square = new Square(new Blue());
 
+
+        //The test shows, when the method draw() is called,
+        // the message on the right side must be the output
         assertEquals(square.draw(), "Square drawn. Color is Blue");
     }
 }
